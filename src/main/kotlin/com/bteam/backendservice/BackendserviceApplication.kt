835435fd0b6624953c230a333763a0a8b634b105
Skippy Levy <levysj@miamioh.edu>
2019-04-08 17:32:47 -0400

    init commit

---
+++ src/main/kotlin/com/bteam/backendservice/BackendserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.bteam.backendservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendserviceApplication>(*args)
+}
